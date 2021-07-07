b6a75c33bad8541e23308f52aa6f6358128366b3
skykiseki <595791833@qq.com>
2021-07-07 20:57:22 +0800

    accomplish TextRank4Keywords
    TextRank4Keywords

---
+++ textrank4ch/TextRank4Keyword.py
@@ -0,0 +1,86 @@
+import jieba.analyse
+import jieba
+import utils
+
+class TextRank4Keywords(object):
+    """
+    提取关键字
+
+    这里直接改造成引用jieba的textrank结果, 不再有逻辑分岔, 且集成了停用词路径等参数
+
+    Attributes:
+    ----------
+    path_stop_words: str, 停用词表路径
+    text: str, 文本字符串
+    is_lower: bool, 是否要转小写字母
+    keywords: 关键字列表, 一个元素包含关键词(必选), 词性(可选), 权重(可选)
+    """
+    def __init__(self):
+        self.path_stop_words = None
+        self.text = None
+        self.is_lower = None
+        self.keywords = None
+
+    def analyze(self, text:str, path_stop_words:str=None, is_lower:bool=True):
+        """
+        对文本进行分析, 找出所有的关键词候选列表
+
+        Parameters:
+        ----------
+        text: str, 文本字符串
+        path_stop_words: str, 停用词表路径
+        is_lower: bool, 是否要转小写字母
+
+        Returns:
+        -------
+        self
+
+        """
+        self.path_stop_words = path_stop_words
+        self.text = text
+        self.is_lower = is_lower
+
+        if self.is_lower:
+            self.text = self.text.strip().replace(" ", "").lower()
+
+        if path_stop_words:
+            jieba.analyse.set_stop_words(stop_words_path=path_stop_words)
+
+        self.keywords = jieba.analyse.textrank(sentence=self.text,
+                                               topK=None,
+                                               allowPOS=utils.allow_pos,
+                                               withWeight=True,
+                                               withFlag=True)
+
+    def get_keywords(self, top_k=6, word_min_len=1, with_weight=True, with_flag=False):
+        """
+        获取需要的关键词
+
+        Parameters:
+        ----------
+        top_k: int, 获取topN权重的词
+        word_min_len: int, 获取大于等于长度阈值的关键词
+        with_weight: bool, 是否要带着权重(PR值)进行输出
+        with_flag: bool, 是否要带着词性进行输出
+
+        Returns:
+        -------
+        关键词列表
+
+        """
+        list_res = []
+
+        words = [item[0].word for item in self.keywords if len(item[0].word) >= word_min_len]
+        weights = [item[1] for item in self.keywords if len(item[0].word) >= word_min_len]
+        flags = [item[0].flag for item in self.keywords if len(item[0].word) >= word_min_len]
+
+        list_res.append(words)
+
+        if with_weight:
+            list_res.append(weights)
+
+        if with_flag:
+            list_res.append(flags)
+
+        return list(zip(*list_res))[:top_k]
+
--- textrank4ch/main.py
+++ textrank4ch/main.py
@@ -1,10 +1,14 @@
 from segment import WordSegment
 import os
-import jieba
-import jieba.analyse
+from TextRank4Keyword import TextRank4Keywords
 
 if __name__ == '__main__':
     work_path = os.getcwd()
-    word_seg = WordSegment(path_stop_words=os.path.join(work_path, 'stop_words.txt'))
-    ss = ["""我是帅哥嗯,哈哈哈哈哈!哈哈哈哈 APP""", """我是帅哥嗯,哈哈哈哈哈!哈哈哈哈 APP"""]
-    print(jieba.analyse.extract_tags('.'.join(ss), withWeight=True, withFlag=True))
+    path_stop_words = os.path.join(work_path, 'stop_words.txt')
+    word_seg = WordSegment(path_stop_words=path_stop_words)
+    ss = "近日，云南省德宏州瑞丽市出现本土新冠肺炎疫情，瑞丽市疫情防控工作指挥部已经发布通告，从7月5日8时起，所有人员非必要不进出瑞丽；自7月6日12时起，将瑞丽市姐告国门社区调整为中风险地区，其他区域为低风险地区。鉴于云南省德宏州疫情变化形势，为严格落实“外防输入、内防反弹”的防控策略，有效控制和降低疫情传播风险，市疾控中心向广大市民发出紧急提醒"
+    t4kw = TextRank4Keywords()
+    t4kw.analyze(text=ss, path_stop_words=path_stop_words)
+
+    print(t4kw.keywords)
+    print(t4kw.get_keywords(with_flag=True))
